edddc45378955340b52a0aa997865abe8b3f5d41
unknown <tlotlegokgatitsoe@gmail.com>
2020-08-19 09:17:00 +0200

    Added countries API

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-console.log( 'Hello ' );
 let deferredPrompt;
 const installButton = document.querySelector( '#installButton' );
 const button = document.querySelector( '.mdc-button' );
 const snackbar = mdc.snackbar.MDCSnackbar.attachTo( document.querySelector( '.mdc-snackbar' ) );
 mdc.ripple.MDCRipple.attachTo( button );
-console.log( 'installButton', installButton );
+console.log( 'installButton', mdc );
 
 window.addEventListener( 'beforeinstallprompt', event => {
     event.preventDefault();
@@ -28,3 +27,21 @@ window.addEventListener( 'appinstalled', event => {
 button.addEventListener( 'click', () => {
     if ( !snackbar.isOpen ) snackbar.open();
 });
+
+
+fetch( 'https://restcountries.eu/rest/v2/all' )
+.then( res => {
+    if ( !res.ok ) throw res.statusText;
+    return res.json();
+})
+.then( countries => {
+    const countriesCardsCont = document.querySelector( '#countries-cards-container' );
+    for ( const country of countries ) {
+        countriesCardsCont.innerHTML += `
+            <div class='mdc-card mdc-card__primary-action'>
+                <div class="mdc-card__media mdc-card__media--16-9" style='background-image: url( "${ country.flag }" );'></div>
+                <h3 class='card-title'> ${ country.name } </h3>
+                <span class='card-sec-text'> ${ new Intl.NumberFormat( 'en').format( country.population ) } people </span>
+            </div>`;
+    }
+})
