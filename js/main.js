15cc4c3e3db68033ca918986ef9590e8834622dd
unknown <tlotlegokgatitsoe@gmail.com>
2020-08-30 10:54:00 +0200

    Added some features

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
-let deferredPrompt;
-const installButton = document.querySelector( '#installButton' );
+
+
 const button = document.querySelector( '.mdc-button' );
 const snackbar = mdc.snackbar.MDCSnackbar.attachTo( document.querySelector( '.mdc-snackbar' ) );
 mdc.ripple.MDCRipple.attachTo( button );
-
-window.addEventListener( 'beforeinstallprompt', event => {
-    event.preventDefault();
-    deferredPrompt = event;
-    installButton.style.display = 'block';
-});
-
-installButton.addEventListener( 'click', () => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then( choiceResult => {
-        deferredPrompt = null;
-        installButton.style.display = 'none';
-    })
-});
 
 button.addEventListener( 'click', () => {
     if ( !snackbar.isOpen ) {
@@ -25,22 +11,22 @@ button.addEventListener( 'click', () => {
 });
 
 
-// fetch( 'https://restcountries.eu/rest/v2/currency/zar?fields=name;population;flag;capital' )
-// .then( res => {
-//     if ( !res.ok ) throw res.statusText;
-//     return res.json();
-// })
-// .then( countries => {
-//     const countriesCardsCont = document.querySelector( '#countries-cards-container' );
-//     for ( const country of countries ) {
-//         countriesCardsCont.innerHTML += `
-//             <div class='mdc-card mdc-card__primary-action'>
-//                 <div class="mdc-card__media mdc-card__media--16-9" style='background-image: url( "${ country.flag }" );'></div>
-//                 <h3 class='card-title'> ${ country.name } </h3>
-//                 <span class='card-sec-text'> ${ new Intl.NumberFormat( 'en').format( country.population ) } people </span>
-//             </div>`;
-//     }
-// })
+fetch( 'https://restcountries.eu/rest/v2/currency/zar?fields=name;population;flag;capital' )
+.then( res => {
+    if ( !res.ok ) throw res.statusText;
+    return res.json();
+})
+.then( countries => {
+    const countriesCardsCont = document.querySelector( '#countries-cards-container' );
+    for ( const country of countries ) {
+        countriesCardsCont.innerHTML += `
+            <div class='mdc-card mdc-card__primary-action'>
+                <div class="mdc-card__media mdc-card__media--16-9" style='background-image: url( "${ country.flag }" );'></div>
+                <h3 class='card-title'> ${ country.name } </h3>
+                <span class='card-sec-text'> ${ new Intl.NumberFormat( 'en' ).format( country.population ) } people </span>
+            </div>`;
+    }
+})
 
 // {flag: "https://restcountries.eu/data/lso.svg", name: "Lesotho", capital: "Maseru", population: 1894194}
 // 1: {flag: "https://restcountries.eu/data/nam.svg", name: "Namibia", capital: "Windhoek", population: 2324388}
