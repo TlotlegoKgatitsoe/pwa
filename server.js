6c5056660308d83426bfdec9e1f7cf845ac53dfb
unknown <tlotlegokgatitsoe@gmail.com>
2020-09-21 14:04:27 +0200

    Added express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require( 'express' );
+const app = express();
+const path = require( 'path' );
+const sourceURL = express.static( path.join( __dirname, 'src' ) );
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use( sourceURL );
+
+// Routing
+app.get( '*', ( req, res ) => {
+  res.sendFile( path.join( sourceURL, 'index.html' ) );
+})
+
+// PORT
+app.listen( PORT, console.log( `app is listening on port ${ PORT }` ) );
